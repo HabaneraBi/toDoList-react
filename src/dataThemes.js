2f8874e0a6e916b36f79ef27232d07a732bbecc5
HabaneraBi <beriya2016@mail.ru>
2025-04-23 23:44:53 +0300

    fix theme

--- src/dataThemes.js
+++ src/dataThemes.js
@@ -3,6 +3,8 @@ const lightTheme = {
   "--all-color": "rgb(247, 247, 247)",
   "--li-color": "black",
   "--placeholder-color": "rgb(195,193,229)",
+  "--active-checkbox": "rgb(238, 237, 255)",
+  "--span-color": "#252525",
 };
 
 const darkTheme = {
@@ -10,6 +12,8 @@ const darkTheme = {
   "--all-color": "rgb(37,37,37)",
   "--li-color": "rgb(247, 247, 247)",
   "--placeholder-color": "rgb(102,102,102)",
+  "--active-checkbox": "rgb(73, 73, 73)",
+  "--span-color": "#979797",
 };
 
 export { lightTheme, darkTheme };
